236bac0756e533f13624d752afe158f69a8636a9
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-10-05 18:49:27 -0300

    criando as primeiras rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
+import { TitleComponent } from './pages/index/title/title.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './pages/portifolio/card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: TitleComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'portifolio',
+    component: CardComponent,
+    pathMatch: 'prefix',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
